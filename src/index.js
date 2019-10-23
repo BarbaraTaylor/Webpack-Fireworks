e820e189ea086670a7b184250c4ed32ef8843853
BarbaraTaylor <btaylor1787@gmail.com>
2019-10-23 13:34:15 -0400

    Fireworks failure-to-pop

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-import main from "./js/main"
+// import main from "./js/main"
 
-main()
+// main() 
+
+
+import Fireworks from "fireworks-canvas"// mjs
+
+const app = document.getElementById("app")
+const fireworks = new Fireworks(app)
+fireworks.start()
--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,32 @@
+//import * as Fireworks from 'fireworks-canvas' 
+import Fireworks from "fireworks-canvas"// mjs
+
 export default () => {
-    alert("it works!")
-  }
+    console.log("something")
+  const app = document.getElementById("app")
+  const fireworks = new Fireworks(app)
+  fireworks.start()
+}
+
+
+// // needs at least a container element, you can provide options
+// // (options are optional, defaults defined below)
+// const container = document.getElementById('container')
+// const options = {
+//   maxRockets: 3,            // max # of rockets to spawn
+//   rocketSpawnInterval: 150, // millisends to check if new rockets should spawn
+//   numParticles: 100,        // number of particles to spawn when rocket explodes (+0-10)
+//   explosionMinHeight: 0.2,  // percentage. min height at which rockets can explode
+//   explosionMaxHeight: 0.9,  // percentage. max height before a particle is exploded
+//   explosionChance: 0.08     // chance in each tick the rocket will explode
+// }
+ 
+// // instantiate the class and call start
+// // this returns a disposable - calling it will stop fireworks.
+// const fireworks = new Fireworks(container, options)
+// const stop = fireworks.start()
+// stop() // stop rockets from spawning
+// fireworks.stop() // also stops fireworks.
+// fireworks.kill() // forcibly stop fireworks
+// fireworks.fire() // fire a single rocket.
+
